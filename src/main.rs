f3b1b33322818805c3fb06755bc488c99e05bb65
HaiqalAly <haiqalaly@gmail.com>
2026-01-20 14:30:25 +0700

    feat: persist FST to disk and read back for search

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use fst::{IntoStreamer, Set};
+use std::fs::File;
+use std::io;
+
+use fst::{IntoStreamer, Set, SetBuilder};
 use fst::automaton::Levenshtein;
 
 // Adapted from the fst crate examples by @burntsushi
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  // A convenient way to create sets in memory.
-  let keys = vec!["fa", "fo", "fob", "focus", "foo", "food", "foul"];
-  let set = Set::from_iter(keys)?;
+  let path = "test.fst";
+  let writer = io::BufWriter::new(File::create(path)?);
+  let mut build = SetBuilder::new(writer)?;
+  let mut keys = vec!["fa", "fo", "fob", "focus", "foo", "food", "foul"];
+  keys.sort();
 
-  // Build our fuzzy query.
+  for key in keys {
+    build.insert(key)?;
+  }
+
+  build.finish()?;
+
+  let data = std::fs::read(path)?;
+  let set = Set::new(data)?;
   let lev = Levenshtein::new("foo", 2)?;
 
-  // Apply our fuzzy query to the set we built.
   let stream = set.search(lev).into_stream();
 
   let keys = stream.into_strs()?;
